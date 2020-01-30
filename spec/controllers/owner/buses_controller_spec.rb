e6bfebdb6d75891821afa28c65a75676cd8e4104
Zamir Manihar <zamir.manihar@gemsessence.com>
2020-01-30 19:23:25 +0530

    test cases for new, show, edit actions in Owner::BusesController

--- spec/controllers/owner/buses_controller_spec.rb
+++ spec/controllers/owner/buses_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Owner::BusesController, type: :controller do
+  let(:user) { User.create(email: "admin@gmail.com", password: '123456', role: "owner", status: "approved", name:"demo") }
+  let (:bus) { owner.buses.create(name: 'demo bus')}
+
+  before(:each) do
+    sign_in user
+  end
+  
   describe "GET index" do
-
-    let(:user) { User.create(email: "admin@gmail.com", password: '123456', role: "owner", status: "approved", name:"demo") }
     let!(:owner) { Owner.create(user: user) }
-    let (:bus) { owner.buses.create(name: 'demo bus')}
-
-    before(:each) do
-      sign_in user
-    end
 
     it "assigns @buses" do
       bus
@@ -23,4 +23,44 @@ RSpec.describe Owner::BusesController, type: :controller do
     end
   end
 
+  describe "GET new" do
+    it "assigns @bus" do
+      get :new
+      expect(assigns(:bus)).to be_a Bus
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET show" do
+    let!(:owner) { Owner.create(user: user) }
+
+    it "assigns @bus" do
+      get :show, params: { id: bus.id }
+      expect(assigns(:bus)).to eq(bus)
+    end
+
+    it "renders the show template" do
+      get :show, params: { id: bus.id }
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET edit" do
+    let!(:owner) { Owner.create(user: user) }
+
+    it "assigns @bus" do
+      get :edit, params: { id: bus.id }
+      expect(assigns(:bus)).to eq(bus)
+    end
+
+    it "renders the edit template" do
+      get :edit, params: { id: bus.id }
+      expect(response).to render_template("edit")
+    end
+  end
+  
 end
