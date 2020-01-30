dc59782c0abd98d81e5152abdf238571d185dd94
Zamir Manihar <zamir.manihar@gemsessence.com>
2020-01-30 17:55:29 +0530

    test case for index action of Owner::BusesController

--- spec/controllers/owner/buses_controller_spec.rb
+++ spec/controllers/owner/buses_controller_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Owner::BusesController, type: :controller do
   describe "GET index" do
+
+    let(:user) { User.create(email: "admin@gmail.com", password: '123456', role: "owner", status: "approved", name:"demo") }
+    let!(:owner) { Owner.create(user: user) }
+    let (:bus) { owner.buses.create(name: 'demo bus')}
+
+    before(:each) do
+      sign_in user
+    end
+
     it "assigns @buses" do
-      bus = Bus.create
+      bus
       get :index
       expect(assigns(:buses)).to eq([bus])
     end
