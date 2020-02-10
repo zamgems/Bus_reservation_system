f1a6d8030c635f1815cf5e83f8e1b73be84f67d4
Zamir Manihar <zamir.manihar@gemsessence.com>
2020-02-10 15:10:26 +0530

    test cases for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+  describe "User model validations" do
+    it "is valid with valid attributes" do
+      expect(User.new(email:'zamirmanihar@yopmail.com', password: '123456')).to be_valid
+    end
+
+    it "is not valid without an email" do
+      expect(User.new(password: '123456', role: 'owner', status: 'approved', name: 'zamir' )).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      expect(User.new(email: 'zamir@gmail.com', role: 'owner', status: 'approved', name: 'zamir' )).to_not be_valid
+    end
+
+    it "is not valid without an invalid email" do
+      expect(User.new(email: 'zamirgmailcom', password: '123456', role: 'owner', status: 'approved', name: 'zamir' )).to_not be_valid
+    end
+
+    it "is not valid when password length is less than 6 character" do
+      expect(User.new(email: 'zamir@gmail.com', password: '12345', role: 'owner', status: 'approved', name: 'zamir' )).to_not be_valid
+    end
+  end
 end
